9627dae627c5e2e476956667d64f690069e6b2fd
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-07-27 23:33:04 +0200

    update sns-sqs stack

--- config/src/sns-sqs-config.ts
+++ config/src/sns-sqs-config.ts
@@ -5,7 +5,9 @@ const topics = {
   tickets_userEventsQueue: `${APP_NAME}-tickets-user-events-queue`,
   tickets_eventEventsQueue: `${APP_NAME}-tickets-event-events-queue`,
   users_ticketEventsQueue: `${APP_NAME}-users-ticket-events-queue`,
+  users_eventEventsQueue: `${APP_NAME}-users-event-events-queue`,
   events_ticketEventsQueue: `${APP_NAME}-events-ticket-events-queue`,
+  events_userEventsQueue: `${APP_NAME}-events-user-events-queue`,
   sqsOfflinePort: 4576
 }
 
